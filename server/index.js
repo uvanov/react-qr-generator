4796bd73e0fbd195a8cde41afd357d84db67ee0d
uvanov <kirill.ivanov000@gmail.com>
2022-02-22 17:09:31 +0200

    Prepare to server start

--- server/index.js
+++ server/index.js
@@ -4,3 +4,11 @@ const qrImage = require("qr-image");
 const fs = require("fs");
 
 const app = express();
+
+app.get("/", (request, response) => {
+   response.send({message: "Hello world!"});
+});
+
+app.listen(5000, () => {
+    console.log("EXPRESS: Server started.") ;
+});
