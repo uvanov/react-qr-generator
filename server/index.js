b1e004c88560ff17ffee26b72e86f15d535c4eba
uvanov <kirill.ivanov000@gmail.com>
2022-02-22 17:43:06 +0200

    Return qr-code directly by rest

--- server/index.js
+++ server/index.js
@@ -11,15 +11,21 @@ app.get("/", (request, response) => {
 
 app.get('/qr', (request, response) => {
 
-    let codeUrl = request.query.url;
+    const codeUrl = request.query.url;
 
     if(codeUrl === undefined || codeUrl.length > 20) {
         response.status(400);
         response.send({ error: "URL is missed, or too long." });
         return;
     }
-    
 
+    let qrCode = qrImage.image(codeUrl, { type: 'svg', size: 6 });
+
+    // qrCode.pipe(require('fs').createWriteStream('results/i_love_qr.svg'));
+    // If i need to store images
+
+    response.setHeader("Content-type", "image/svg");
+    qrCode.pipe(response);
 });
 
 app.listen(5000, () => {
