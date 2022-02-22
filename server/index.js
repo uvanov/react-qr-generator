e045a2cf19b27bdc2666f768a89573e4e15a9397
uvanov <kirill.ivanov000@gmail.com>
2022-02-22 17:25:59 +0200

    Check if url-parameter is incorrect

--- server/index.js
+++ server/index.js
@@ -9,6 +9,19 @@ app.get("/", (request, response) => {
    response.send({message: "Hello world!"});
 });
 
+app.get('/qr', (request, response) => {
+
+    let codeUrl = request.query.url;
+
+    if(codeUrl === undefined || codeUrl.length > 20) {
+        response.status(400);
+        response.send({ error: "URL is missed, or too long." });
+        return;
+    }
+    
+
+});
+
 app.listen(5000, () => {
     console.log("EXPRESS: Server started.") ;
 });
