116eaf419205d34b952b0cb41341024ddf3268ea
uvanov <kirill.ivanov000@gmail.com>
2022-02-25 12:58:54 +0200

    Add qrgenerator class methods

--- server/QRGenerator.js
+++ server/QRGenerator.js
@@ -1,17 +1,28 @@
 const qr = require("qr-image");
+const filesystem = require("fs");
+const path = require("path");
 
 class QRGenerator {
 
-    #GenerateFilename(){
+    constructor() {
+        this.format = "svg";
+    }
+
+    GenerateFilename(){
         return ( Math.random() + 1 + Date.now() ).toString(36);
     }
 
-    #saveFile(){
+    SaveFile(QRCode){
+        const fileName = this.GenerateFilename();
+        const filePath = path.join(__dirname, "results", `${fileName}.${this.format}`);
 
+        QRCode.pipe( filesystem.createWriteStream(filePath) );
+        return filePath;
     }
 
-   createCode( linkToUrl ){
-
+   CreateCode( linkToUrl, codeSize ){
+        const code = qr.image(linkToUrl, {size: codeSize});
+        return this.SaveFile(code);
    }
 
 
